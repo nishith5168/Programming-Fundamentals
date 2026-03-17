9b7b0256f1b9f7de377589380c8d86688fbbb7d8
nishith5168 <nishith.uk@gmail.com>
2026-03-17 14:53:01 +0530

    Adding Comments in Python

--- Lab 2 - Codes/number_1.py
+++ Lab 2 - Codes/number_1.py
@@ -1,2 +1,6 @@
 print("Nishith Siribaddana")
 print("Royal Institute Nugegoda")
+#This is a single line comment.
+"""This is a multi line comment.
+This is the second line of the multi line comment."""
+
